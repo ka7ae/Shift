b939a071d3b0f043fe7dbe1e5f9b94c24bf7b00f
ka7ae <hana.caelum@gmail.com>
2024-07-17 11:26:54 +0900

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -39,7 +39,9 @@ SUPERUSER_PASSWORD = env("SUPERUSER_PASSWORD")
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'daily-report.onrender.com']
+
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
