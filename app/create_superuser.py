2d566c14cd2aedec80873671747d63e41be7fc69
ka7ae <hana.caelum@gmail.com>
2024-10-15 13:24:58 +0900

    create_superuser create

---
+++ app/create_superuser.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from django.conf import settings
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if not User.objects.filter(account_id=settings.SUPERUSER_NAME).exists():
+            User.objects.create_superuser(
+                account_id=settings.SUPERUSER_ACCOUNT_ID,
+                last_name=settings.SUPERUSER_LAST_NAME,
+                first_name=settings.SUPERUSER_FIRST_NAME,
+                password=settings.SUPERUSER_PASSWORD
+            )
+            print("スーパーユーザー作成")
