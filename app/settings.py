811373c050c3bbdddbcea7ab929d2bfdea3ee8ae
ka7ae <hana.caelum@gmail.com>
2024-10-15 11:54:11 +0900

    10/15 ignore commit

--- app/settings.py
+++ app/settings.py
@@ -29,18 +29,18 @@ env.read_env(os.path.join(BASE_DIR, ".env"))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ('django-insecure-m_&@%)em=c5a+nb5bjisrt!zsshyh&52bw@drtns9#az+$*4+v')
+
 
 SUPERUSER_NAME = env("SUPERUSER_NAME")
 SUPERUSER_PASSWORD = env("SUPERUSER_PASSWORD")
+SECRET_KEY = env('SECREST_KEY')
 
 
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'Unagi-machida.shift.com']
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
